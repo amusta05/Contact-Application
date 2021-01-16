f49f3bb68f2bf8ee0aa865aaf94ddc35664822a1
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-15 19:17:01 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+
+// contains all the libaries that will be for the backend
+const express  = require('express');
+
+// initalize express
+const app = express();
+
+// varaible for the PORT number
+const PORT = process.env.PORT || 3000;
+
+// adding a endpoint to the home page
+app.get('/', (req,res) => res.json({msg: 'Welcome to the Contact Application'}) );
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
