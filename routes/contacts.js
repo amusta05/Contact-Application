44375a2e60a891823647addc8f8efd733b02931b
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-17 19:51:07 -0500

    saved contact to the database for each account

--- routes/contacts.js
+++ routes/contacts.js
@@ -17,8 +17,32 @@ router.get('/',auth, async (req,res)=>{
 });
 
 // Route to add a new  contact
-router.post('/',(req,res)=>{
-    res.send("Add new contact" );
+router.post('/',[auth,[
+    check('name','Name is required').not().isEmpty()
+]], async (req,res)=>{
+    const errors  = validationResult(req);
+    if (!errors.isEmpty()){
+        // returns an error if any of the validation is not met
+        return res.status(400).json({errors: errors.array() });
+    }
+    const {name,email,phone, type} = req.body;
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+        const contact = await newContact.save();
+        res.json(contact);
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
 });
 
 // Route to update a contact
