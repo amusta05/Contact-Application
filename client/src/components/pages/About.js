467e9a050deafa19867819bd45d87c2ee8f01d18
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-19 17:32:37 -0500

    added navbar links and a about page

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this Contact Application</h1>
+            <p className="my-1">
+                This is a full stack application with front-end covered in React and backend in Node.js.
+                This allows users to store contacts, edit them and delete them.
+            </p>
+            <p className="bg-dark p">
+                <strong>Version: </strong>1.0.0
+            </p>
+        </div>
+    )
+}
+export default About
