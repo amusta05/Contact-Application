b85c347748a9806566e5686f8e5ab5a06015ebba
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-21 15:07:07 -0500

    filter and clear contact working on the UI

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,27 @@
+import React, {useContext, useRef,useEffect} from 'react'
+import ContactContext from '../../context/contact/contactContext';
+const ContactFilter = () => {
+    const contactContext = useContext(ContactContext);
+    const text = useRef('');
+
+    useEffect(() =>{
+        if(contactContext.filtered == null){
+            text.current.value = '';
+        }
+    });
+    const onChange = e =>{
+        
+        if(text.current.value !== ''){
+            contactContext.filterContact(e.target.value);
+        }else{
+            contactContext.clearFilter();
+        }
+    }
+    return (
+        <form>
+            <input type="text" ref={text} type="text" placeholder="Filter Contacts..." onChange={onChange} />
+            
+        </form>
+    )
+}
+export default ContactFilter;
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -5,6 +5,7 @@ import {
     CLEAR_CURRENT,
     UPDATE_CONTACT,
     FILTER_CONTACT,
+    FILTER_CONTACTS,
     CLEAR_FILTER
 } from '../types'
 
@@ -36,7 +37,7 @@ export default (state,action) => {
                 ...state,
                 contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
             };
-        case FILTER_CONTACT:
+        case FILTER_CONTACTS:
             return {
                 ...state,
                 filtered: state.contacts.filter(contact =>{
