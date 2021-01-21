326cb110bc3662755ba74cd1038177600ad09b02
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-21 16:26:29 -0500

    No changes

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,20 +1,32 @@
-import React, { Fragment , useContext} from 'react'
+import React, { Fragment , useContext,useRef} from 'react'
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem'
+import { CSSTransition , TransitionGroup} from 'react-transition-group';
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
     const {contacts,filtered} = contactContext;
-
+    const nodeRef = useRef(null)
     if(contacts.length === 0){
         return(
             <h4>Please enter a contact.</h4>
         );
     }
+
     return (
-        <Fragment>
-            {filtered !== null ? filtered.map(contact => (<ContactItem key= {contact.id} contact={contact}/>)): contacts.map(contact => <ContactItem key= {contact.id} contact={contact}/>)}
-           
-        </Fragment>
+      <Fragment>
+        
+          {filtered !== null
+            ? filtered.map((contact) => (
+              
+                  <ContactItem key={contact.id}  contact={contact} />
+              ))
+            : contacts.map((contact) => (
+                
+                  <ContactItem key={contact.id} contact={contact} />
+            
+              ))}
+      
+      </Fragment>
     );
 }
 export default Contacts;
