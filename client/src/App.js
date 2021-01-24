60781eba67f84cbdc9cba1da8c32cf9647291a68
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-24 17:41:21 -0500

    worked on adding error checking

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,18 @@ import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
 const App = () => {
   return (
     <AuthState>
       <ContactState>
+        <AlertState>
         <Router>
           <Fragment>
             <Navbar />
             <div className="container">
+              <Alerts />
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
@@ -25,6 +29,7 @@ const App = () => {
             </div>
           </Fragment>
         </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
