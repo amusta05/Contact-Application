a82b93da5be592ca7f97d0abc37cb3c90d905c25
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-29 18:36:58 -0500

    removed inital endpoint

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use(express.json({extended: false}));
 const PORT = 5000;
 
 // adding a endpoint to the home page
-app.get('/', (req,res) => res.json({msg: 'Welcome to the Contact Application'}) );
 
-i
+
+
 // Define routes
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/contacts',require('./routes/contacts'));
-f(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     app.get('*',(req,res) =>
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
