ee3e73425ab5b3397e76bd2cb50d4d0c5dd571e7
Areeb Mustafa <areebmustafa@Areebs-MacBook-Air.local>
2021-01-29 18:31:10 -0500

    Prepare to deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express  = require('express');
 const connect  = require('mongoose');
 const connectDB = require('./config/db');
-
+const path = require('path')
 // initalize express
 const app = express();
 
@@ -18,10 +18,17 @@ const PORT = 5000;
 // adding a endpoint to the home page
 app.get('/', (req,res) => res.json({msg: 'Welcome to the Contact Application'}) );
 
-
+i
 // Define routes
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/contacts',require('./routes/contacts'));
+f(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) =>
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    );
+
+}
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
